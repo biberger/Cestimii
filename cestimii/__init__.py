8d5a0c3803866c89b543fd9652789fac49a1fd93
Simon Biberger <biberger@ebi.ac.uk>
2021-09-20 15:59:34 +0200

    repo should now a proper python package

---
+++ cestimii/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['curvature', 'geomshapes','occupancygrids','split']
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Cestimii',
+    url='https://github.com/uhlmanngroup/Cestimii',
+    author='Simon Biberger',
+    author_email='cestimii@biberger.xyz',
+    packages=['cestimii'],
+    install_requires=['numpy','scipy','matplotlib','napari','h5py','pillow'],
+    version='0.6',
+    license='BSD 3',
+    description='Multiscale Curvature Estimation using Integral Invariants.',
+    long_description=open('README.txt').read(),
+    scripts = ['scripts/test.py']
+)
